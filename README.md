ba38ec5dbd8c9b0688e4c764c3433cc58a911724
Joke De Winter <joke@jokedewinter.co.uk>
2015-06-11 09:52:20 +0100

    Fix for the double click it needed to work. Also a resizing fix.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # show-hide-nav-js
-Show / Hide a menu using only pure JavaScript
+Show / Hide a menu using only pure JavaScript, no jQuery. Because sometimes there is no need to load the whole of jQuery just for a menu like that.
+
+I can't claim all the credit for this, found help here: [jsFiddle](http://jsfiddle.net/fyUJc/31/).
+
+###Update June 11, 2015
+Added a fix for the show menu button. It needed clicking twice for the magic to kick in. 
+If the viewport is below 600px, the menu is set to display: none.
+
+If the viewport is resized from wide to narrow, the menu might disappear.
+I have added an event listener to fix this.
--- js/navigation.js
+++ js/navigation.js
@@ -6,8 +6,29 @@
  * -------------------------------------------------------------------------------------------------
  */
 
+// The show menu button
 var show_menu = document.querySelector('.show_menu_btn');
 
+// Set the nav to display:none manually, to avoid having to click the button twice.
+// But only if the viewport is below 600px.
+var viewport_width = document.documentElement.clientWidth;
+var menu = document.querySelector('#nav ul');
+if ( viewport_width < 600 ) { menu.style.display = "none"; }
+
+// If a window is resized from narrow to wide the menu may disappear.
+// Avoid this.
+window.addEventListener('resize', function(event) {
+
+	var new_viewport = document.documentElement.clientWidth;
+
+	if ( new_viewport > 599 ) {
+		menu.style.display = "block";
+	} else {
+		menu.style.display = "none";
+	}
+});
+
+// Make the menu show/hide by the click of a button
 show_menu.addEventListener('click', function(event) {
     var target = document.querySelector(show_menu.getAttribute('data-target'));
 
